dc3c19c4ccada89d1a0b162021934c9b213491fc
Krzysztof Kampa <krzysztof.kampa@trimlogic.pl>
2020-09-21 13:47:28 +0200

    Todo.java & TodoRepository.java interface added

---
+++ src/main/java/com/example/demo/Todo.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String text;
+
+    private Boolean done;
+
+    public Todo() {
+    }
+
+    public Todo(String text, Boolean done) {
+        this.text = text;
+        this.done = done;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Boolean getDone() {
+        return done;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+}
