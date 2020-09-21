393442ada8e3f167d1510d92b06df869029ff574
Krzysztof Kampa <krzysztof.kampa@trimlogic.pl>
2020-09-21 13:22:59 +0200

    TodoLayout.java added, TodoUI methods logic-filled

---
+++ src/main/java/com/example/demo/TodoLayout.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+import com.vaadin.spring.annotation.SpringComponent;
+import com.vaadin.ui.VerticalLayout;
+
+@SpringComponent
+public class TodoLayout extends VerticalLayout {
+}
--- src/main/java/com/example/demo/TodoUI.java
+++ src/main/java/com/example/demo/TodoUI.java
@@ -2,13 +2,16 @@ package com.example.demo;
 
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringUI
 public class TodoUI extends UI {
 
     private VerticalLayout root;
+
+    @Autowired
+    TodoLayout todoLayout;
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
@@ -20,22 +23,30 @@ public class TodoUI extends UI {
     }
 
     private void setupLayout() {
-
+        root = new VerticalLayout();
+        setContent(root);
     }
 
     private void addHeader() {
-
+        root.addComponent(new Label("TODO's"));
     }
 
     private void addForm() {
+        HorizontalLayout formLayout = new HorizontalLayout();
 
+        TextField task = new TextField();
+        Button add = new Button("Add");
+
+        formLayout.addComponents(task, add);
+
+        root.addComponent(formLayout);
     }
 
     private void addTodoList() {
-
+        root.addComponent(todoLayout);
     }
 
     private void addDeleteButton() {
-
+        root.addComponent(new Button("Delete completed"));
     }
 }
