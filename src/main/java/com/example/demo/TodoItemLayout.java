1f78f39e3dd6a2d364b70fa783b44c97b9a12357
Krzysztof Kampa <krzysztof.kampa@trimlogic.pl>
2020-09-22 09:28:19 +0200

    TodoItemLayout.java added, TodoLayout updated and initialized with init method

---
+++ src/main/java/com/example/demo/TodoItemLayout.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import com.vaadin.ui.CheckBox;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.TextField;
+
+public class TodoItemLayout extends HorizontalLayout {
+
+    private final CheckBox done;
+    private final TextField text;
+
+    public TodoItemLayout(Todo todo) {
+        done = new CheckBox();
+        text = new TextField();
+
+        addComponents(done, text);
+    }
+}
--- src/main/java/com/example/demo/TodoLayout.java
+++ src/main/java/com/example/demo/TodoLayout.java
@@ -2,7 +2,23 @@ package com.example.demo;
 
 import com.vaadin.spring.annotation.SpringComponent;
 import com.vaadin.ui.VerticalLayout;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
 
 @SpringComponent
 public class TodoLayout extends VerticalLayout {
+
+    @Autowired
+    TodoRepository repo;
+
+    @PostConstruct
+    void init() {
+        setTodos(repo.findAll());
+    }
+
+    private void setTodos(List<Todo> todos) {
+        todos.forEach(todo -> addComponent(new TodoItemLayout(todo)));
+    }
 }
