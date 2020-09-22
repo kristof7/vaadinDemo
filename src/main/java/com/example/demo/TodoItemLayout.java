3652f380df2c7eda978c836107f4df1bed618154
Krzysztof Kampa <krzysztof.kampa@trimlogic.pl>
2020-09-22 09:37:07 +0200

    TodoItemLayout update - add Binder and set beans

--- src/main/java/com/example/demo/TodoItemLayout.java
+++ src/main/java/com/example/demo/TodoItemLayout.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
+import com.vaadin.data.Binder;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.CheckBox;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.TextField;
+import com.vaadin.ui.themes.ValoTheme;
 
 public class TodoItemLayout extends HorizontalLayout {
 
@@ -12,7 +15,14 @@ public class TodoItemLayout extends HorizontalLayout {
     public TodoItemLayout(Todo todo) {
         done = new CheckBox();
         text = new TextField();
+        text.addStyleName(ValoTheme.TEXTAREA_BORDERLESS);
 
-        addComponents(done, text);
+        addComponents(done);
+        addComponentsAndExpand(text);
+        setDefaultComponentAlignment(Alignment.MIDDLE_LEFT);
+
+        Binder<Todo> binder = new Binder<>(Todo.class);
+        binder.bindInstanceFields(this);
+        binder.setBean(todo);
     }
 }
